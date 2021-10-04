df3fb337cc38e303b7357a43b1fc698a5f307f3f
teencoderPro <85505529+teencoderPro@users.noreply.github.com>
2021-10-04 16:38:12 +0530

    Update Form.js

--- Form.js
+++ Form.js
@@ -1,23 +1,104 @@
 class Form {
      constructor() {
+      this.input = createInput("");
+      this.button = createButton('CONTINUE');
+      
+ 
+    }
+
+     hide(){
+      this.title.hide();
+       this.button.hide();
+       this.input.hide();
        
      }
+
    
      display(){
        var title = createElement('h2')
        title.html("REGISTER PLAYER NAME");
        title.position(130,0);
 
-       this.input = createInput("Name");
-       this.button = createButton('CONTINUE');
-
        this.input.position(130,160);
        this.button.position(250,200);
 
-       
+       this.button.mousePressed(()=>{
+        this.input.hide();
+        this.button.hide();
+        
 
-       
-   
+        player.name = this.input.value();
+
+        title.html("CHOOSE THE GAME MODE");
+        title.position(130,0);
+
+        this.pistol = createButton('PISTOL');
+       this.SmgLmg = createButton('SMG & LMG');
+       this.sniper = createButton('SNIPER');
+
+       this.pistol.position(250,150);
+       this.SmgLmg.position(250,200);
+       this.sniper.position(250,250);
+
+       this.pistol.mousePressed(()=>{
+        this.pistol.hide();
+        this.SmgLmg.hide();
+        this.sniper.hide();
+
+        title.html("CHOOSE THE GAME MAP");
+        title.position(130,0);
+
+        this.bg1 = createButton('bg1');
+       this.bg2 = createButton('bg2');
+       this.bg3 = createButton('bg3');
+
+       this.bg1.position(250,150);
+       this.bg2.position(250,200);
+       this.bg3.position(250,250);
+
+       });
+
+       this.SmgLmg.mousePressed(()=>{
+        this.pistol.hide();
+        this.SmgLmg.hide();
+        this.sniper.hide();
+
+        title.html("CHOOSE THE GAME MAP");
+        title.position(130,0);
+
+        this.bg1 = createButton('bg1');
+       this.bg2 = createButton('bg2');
+       this.bg3 = createButton('bg3');
+
+       this.bg1.position(250,150);
+       this.bg2.position(250,200);
+       this.bg3.position(250,250);
+
+       });
+
+       this.sniper.mousePressed(()=>{
+        this.pistol.hide();
+        this.SmgLmg.hide();
+        this.sniper.hide();
+
+        title.html("CHOOSE THE GAME MAP");
+        title.position(130,0);
+
+        this.bg1 = createButton('bg1');
+       this.bg2 = createButton('bg2');
+       this.bg3 = createButton('bg3');
+
+       this.bg1.position(250,150);
+       this.bg2.position(250,200);
+       this.bg3.position(250,250);
+
+       });
+
+       });
      }
+
+
+
+     
    }
-   
+  
